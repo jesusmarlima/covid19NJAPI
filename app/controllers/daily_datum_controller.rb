1625d62913bcbbdb7517d2aa077f1a53daf0ab5c
jesusmarlima <jesusmarlima@gmail.com>
2020-04-10 15:40:55 -0400

    add more methods

--- app/controllers/daily_datum_controller.rb
+++ app/controllers/daily_datum_controller.rb
@@ -4,4 +4,22 @@ class DailyDatumController < ApplicationController
       {dailyData: DailyDatum.where("to_char(date,'YYYY-MM-DD') = '#{(DateTime.yesterday)}'")}
     render json: data
   end
+  def state
+    data =
+      {dailyData: DailyDatum.where(owner: "NJ")}
+    render json: data
+  end
+
+  def essex
+    data =
+      {dailyData: DailyDatum.where(owner: "Essex")}
+    render json: data
+  end
+
+  def all
+    data =
+      {dailyData: DailyDatum.all}
+    render json: data
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/today', to: 'daily_datum#today'
+  get '/state', to: 'daily_datum#state'
+  get '/essex', to: 'daily_datum#essex'
+  get '/all', to: 'daily_datum#all'
 end
