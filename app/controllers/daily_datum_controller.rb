383edb0f047d6c7d830cdc939736bf200a427b04
jesusmarlima <jesusmarlima@gmail.com>
2020-04-14 21:09:04 -0400

    add growth data

--- app/controllers/daily_datum_controller.rb
+++ app/controllers/daily_datum_controller.rb
@@ -22,4 +22,30 @@ class DailyDatumController < ApplicationController
     render json: data
   end
 
+  def growthNJ
+    njs_calculated = []
+    njs = DailyDatum.where(owner: "NJ")
+    njs.each_with_index do | daily, index |
+      x = njs[index + 1].present? ? njs[index + 1].positive.to_f : 0.to_f
+      val =  (x - daily.positive.to_f) / daily.positive.to_f
+      njs_calculated << { day: index, value: val*100 }
+    end
+    data = {dailyData: njs_calculated[0..njs_calculated.length-2]}
+
+    render json: data
+  end
+
+  def growthEssex
+    essex_calculated = []
+    essex = DailyDatum.where(owner: "Essex")
+    essex.each_with_index do | daily, index |
+      x = essex[index + 1].present? ? essex[index + 1].positive.to_f : 0.to_f
+      val =  (x - daily.positive.to_f) / daily.positive.to_f
+      essex_calculated << { day: index, value: val*100 }
+    end
+    data = {dailyData: essex_calculated[0..essex_calculated.length-2]}
+
+    render json: data
+  end
+
 end
