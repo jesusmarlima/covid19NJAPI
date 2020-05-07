1f6db4ffe3f7e837bfef996c0ad5b148c7e12761
jesusmarlima <jesusmarlima@gmail.com>
2020-05-07 12:10:51 -0400

    fix message

--- app/models/daily_datum.rb
+++ app/models/daily_datum.rb
@@ -1,8 +1,7 @@
 class DailyDatum < ApplicationRecord
   after_create :send_notification
-  attr_reader :deaths
 
   def send_notification
-    NotificationService.fcm_push_notification("new cases entered. Deaths #{@deaths ||= 0}")
+    NotificationService.fcm_push_notification("new cases entered. Deaths #{self.deaths ||= 0}")
   end
 end
